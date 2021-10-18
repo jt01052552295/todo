8e07f2f54e9647b99495ce5b283ad5341942686d
jt01052552295 <jt01052552295@gmail.com>
2021-10-18 21:18:45 +0900

    Scout
    meilisearch

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Todo extends Model
 {
-    use HasFactory;
-    protected $fillable = [
-      'title',
-      'content',
-      'deadline',
-      'isDone'
-    ];
+  use HasFactory;
+  use Searchable;
+
+  protected $fillable = [
+    'title',
+    'content',
+    'deadline',
+    'isDone'
+  ];
 }
